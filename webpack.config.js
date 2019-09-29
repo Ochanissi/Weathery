34c00a5f3bb318218559c34830585fdc96470e7c
Ochanissi <mirelalex2094@gmail.com>
2019-09-29 21:35:52 +0300

    modified the webpack for scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const autoprefixer = require("autoprefixer");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
+    entry: ['babel-polyfill', './src/js/index.js', "./src/sass/main.scss"],
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
     },
     devServer: {
         contentBase: './dist'
@@ -14,6 +16,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            path: path.resolve(__dirname, 'dist/css'),
+            filename: "bundle.css"
         })
     ],
     module: {
@@ -24,6 +30,16 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             }
         ]
     }
