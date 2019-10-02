359e63e6dc37d282c932f537b9a975c44cf873ae
Ochanissi <mirelalex2094@gmail.com>
2019-10-02 21:43:19 +0300

    making the first api call & setting up the project

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '985bae1e1ba4fca94db72ba79093602d';
+    try {
+        const res = await axios(`${proxy}https://api.darksky.net/forecast/${key}/${query},-71.0589`);
+        const temps = res.data.currently;
+        console.log(res);
+        console.log(temps);
+    } catch(error) {
+        alert(error);
+    }
+}
+
+getResults(42.3601);
+
