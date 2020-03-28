bf25af02de9f363be336438d202a7224af8a0186
Mirel Home <mirelalex2094@gmail.com>
2020-03-28 10:42:53 +0200

    clear console logs

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,65 +1,65 @@
-import axios from 'axios';
-import { keyDarkSky, proxy } from '../config';
+import axios from "axios";
+import { keyDarkSky, proxy } from "../config";
 
 export default class Search {
-    constructor(query_lat, query_long) {
-        this.query_lat = query_lat;
-        this.query_long = query_long;
+  constructor(query_lat, query_long) {
+    this.query_lat = query_lat;
+    this.query_long = query_long;
+  }
+
+  async getResults() {
+    try {
+      const res = await axios(
+        `${proxy}https://api.darksky.net/forecast/${keyDarkSky}/${this.query_lat},${this.query_long}?units=si&exclude=minutely,flags,alerts`
+      );
+      this.result = res.data;
+      this.currently = res.data.currently;
+      this.hourly = res.data.hourly.data;
+      this.daily = res.data.daily.data;
+
+      // console.log(res);
+      // console.log(this.result);
+      // console.log(this.result.currently);
+      // console.log(this.result.hourly.data);
+      // console.log(this.result.daily.data);
+    } catch (error) {
+      alert(error);
     }
+  }
 
-    async getResults() {
-        try {
-            const res = await axios(`${proxy}https://api.darksky.net/forecast/${keyDarkSky}/${this.query_lat},${this.query_long}?units=si&exclude=minutely,flags,alerts`);
-            this.result = res.data;
-            this.currently = res.data.currently;
-            this.hourly = res.data.hourly.data;
-            this.daily = res.data.daily.data;
-            
-            console.log(res);
-            // console.log(this.result);
-            // console.log(this.result.currently);
-            // console.log(this.result.hourly.data);
-            // console.log(this.result.daily.data);
-        } catch (error) {
-            alert(error);
-        }
+  changeUnits(bool) {
+    if (bool) {
+      // console.log(bool);
+
+      this.currently.temperature = this.currently.temperature * 1.8 + 32;
+      this.hourly.forEach(x => {
+        x.temperature = x.temperature * 1.8 + 32;
+      });
+
+      this.currently.windSpeed *= 0.62137;
+
+      this.daily.forEach(x => {
+        x.windSpeed *= 0.62137;
+
+        x.temperatureHigh = x.temperatureHigh * 1.8 + 32;
+        x.temperatureLow = x.temperatureLow * 1.8 + 32;
+      });
+    } else {
+      // console.log(bool);
+
+      this.currently.temperature = (this.currently.temperature - 32) / 1.8;
+      this.hourly.forEach(x => {
+        x.temperature = (x.temperature - 32) / 1.8;
+      });
+
+      this.currently.windSpeed /= 0.62137;
+
+      this.daily.forEach(x => {
+        x.windSpeed /= 0.62137;
+
+        x.temperatureHigh = (x.temperatureHigh - 32) / 1.8;
+        x.temperatureLow = (x.temperatureLow - 32) / 1.8;
+      });
     }
-
-    changeUnits(bool) {
-        if (bool) {
-            // console.log(bool);
-
-            this.currently.temperature = this.currently.temperature *  1.8 + 32;
-            this.hourly.forEach(x => {
-                x.temperature = x.temperature *  1.8 + 32;
-            });
-
-            this.currently.windSpeed *= 0.62137;
-            
-            this.daily.forEach(x => {
-                x.windSpeed *= 0.62137;
-
-                x.temperatureHigh = x.temperatureHigh *  1.8 + 32;
-                x.temperatureLow = x.temperatureLow *  1.8 + 32;
-            });
-
-
-        } else {
-            // console.log(bool);
-
-            this.currently.temperature = (this.currently.temperature - 32) / 1.8;
-            this.hourly.forEach(x => {
-                x.temperature = (x.temperature - 32) / 1.8;
-            });
-
-            this.currently.windSpeed /= 0.62137;
-
-            this.daily.forEach(x => {
-                x.windSpeed /= 0.62137;
-
-                x.temperatureHigh = (x.temperatureHigh - 32) / 1.8;
-                x.temperatureLow = (x.temperatureLow - 32) / 1.8;
-            });
-        }
-    }
+  }
 }
