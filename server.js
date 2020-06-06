172e3bf8900d6a1b5ba901133ef10d6767ca7645
Mirel Home <mirelalex2094@gmail.com>
2020-06-06 22:06:03 +0300

    added express to serve the files in nodejs

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+const enforce = require('express-sslify');
+
+// App initialization
+const app = express();
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+  // Serving static files
+  app.use(express.static(path.join(__dirname, 'dist')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+}
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
+});
