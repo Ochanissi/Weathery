fe1aff1cd3c817e6378b5e4de91ea6a8467d982c
root <root@Ubuntu-18.04>
2020-06-06 21:30:32 +0000

    aaa

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const enforce = require('express-sslify');
 
 // App initialization
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(compression());
